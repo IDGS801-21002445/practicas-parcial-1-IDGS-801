953b67d5aae7551c77deafe60c3725cdafe87142
IDGS801-21002445 <riverhernan16idgs@gmail.com>
2024-01-24 19:37:09 -0600

    24701/2024 JARH

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index() :
+    return render_template("index.html")
+
+@app.route("/resultado", methods=["GET","POST"])
+def resultado() :
+
+    if request.methods == "POST" :
+        numero1 = request.form.get("n1")
+        numero2 = request.form.get("n2")
+        res_suma = numero1 + numero2
+        res_resta = numero1 - numero2
+        res_multiplicacion = numero1 * numero2
+        res_division = numero1 / numero2
+
+        if request.form.get("suma") :
+            return "La suma de {} + {} da como resultado {}".format(numero1, numero2, res_suma)
+        elif request.form.get("resta") :
+            return "La suma de {} + {} da como resultado {}".format(numero1, numero2, res_resta)
+        elif request.form.get("multiplicacion") :
+            return "La suma de {} + {} da como resultado {}".format(numero1, numero2, res_multiplicacion)
+        elif request.form.get("division") :
+            return "La suma de {} + {} da como resultado {}".format(numero1, numero2, res_division)
+    return "No aplica"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
